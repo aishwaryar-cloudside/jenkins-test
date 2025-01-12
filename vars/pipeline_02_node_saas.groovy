04809bcac869ccddfac36519b9af1238b4ecf8a8
aishwaryar-cloudside <137072334+aishwaryar-cloudside@users.noreply.github.com>
2025-01-12 16:49:52 +0530

    Update pipeline_02_node_saas.groovy

--- vars/pipeline_02_node_saas.groovy
+++ vars/pipeline_02_node_saas.groovy
@@ -169,6 +169,14 @@ def call(Map params) {
                         sh '''
                         #!/bin/bash
                         gsutil cp gs://${GCS_BUCKET}/${PROJECT_ID}/${ACCOUNT}/${REPO_NAME}/deployment.yaml .
+
+                        echo "GCP_REGISTRY=${GCP_REGISTRY}"
+                        echo "PROJECT_ID=${PROJECT_ID}"
+                        echo "GCP_REPOSITORY=${GCP_REPOSITORY}"
+                        echo "REPO_NAME=${REPO_NAME}"
+                        echo "GCP_DOCKER_TAG=${GCP_DOCKER_TAG}"
+
+                        
                         sed -i "s|image:.*|image: ${GCP_REGISTRY}/${PROJECT_ID}/${GCP_REPOSITORY}/${REPO_NAME}:${GCP_DOCKER_TAG}|" deployment.yaml
 
                         mv deployment.yaml deployment-${BUILD_NUMBER}.yaml
