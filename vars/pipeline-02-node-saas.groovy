440e4053cbd16d1b94d4b53948be580eac63c788
aishwaryar-cloudside <137072334+aishwaryar-cloudside@users.noreply.github.com>
2025-01-22 10:55:51 +0530

    Update pipeline-02-node-saas.groovy

--- vars/pipeline-02-node-saas.groovy
+++ vars/pipeline-02-node-saas.groovy
@@ -1,15 +1,1 @@
-import requests
 
-def send_notification():
-    webhook_url = "<WEBHOOK_URL>"
-    message = {
-        "text": "Bitbucket pipeline executed successfully!"
-    }
-    response = requests.post(webhook_url, json=message)
-    if response.status_code == 200:
-        print("Notification sent successfully!")
-    else:
-        print(f"Failed to send notification: {response.status_code}")
-
-if __name__ == "__main__":
-    send_notification()
